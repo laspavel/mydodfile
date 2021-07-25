648a7a3af08167febdf048ce46dfc5f0f056463f
laspavel <laspavel@gmail.com>
2021-07-25 10:42:32 +0300

    Init repo

---
+++ backup.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+STARTDIR="$(pwd)"
+
+PASS=`cat .pass`
+
+dconf dump /org/gnome/shell/extensions/dash-to-panel/ > dtp_settings
+cp -rf ~/.vimrc $STARTDIR
+cp -rf ~/.viminfo $STARTDIR
+cp -rf ~/.toprc $STARTDIR
+cp -rf ~/.tmux $STARTDIR
+cp -rf ~/.python_history $STARTDIR
+cp -rf ~/.nanorc  $STARTDIR
+cp -rf ~/.lesshst $STARTDIR
+cp -rf ~/.gitconfig $STARTDIR
+cp -rf ~/.bashrc $STARTDIR
+cp -rf ~/.bash_profile $STARTDIR
+cp -rf ~/.bash_logout $STARTDIR
+cp -rf ~/.bash_history $STARTDIR
+cp -rf ~/.ssh $STARTDIR
+
+zip -P$PASS -9 -q -r -m ./.ssh.zip ./.ssh
+zip -P$PASS -9 -q -r -m ./.tmux.zip ./.tmux
+
+git add .
+git commit -a -m "new backup"
+autotag
+
+
+
+
